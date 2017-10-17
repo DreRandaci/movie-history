b9588d445751aec61bf948e6f37cff7f0f7eac1c
Dre mf Randaci <drerandaci@gmail.com>
2017-10-16 21:25:31 -0500

    movies will display as cards when printed

---
+++ javascripts/dom.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const domString = (movies) => {
+    let str = '';
+    movies.forEach((mv, i) => {
+        if (i % 3 === 0) {
+            str += `<div class="row">`;
+        }
+        str += `<div class="col-sm-6 col-md-4">`;
+        str +=   `<div class="thumbnail">`;
+        str +=     `<img src="${mv.poster_path}" alt="">`;
+        str +=     `<div class="caption">`;
+        str +=       `<h3>${mv.title}</h3>`;
+        str +=       `<p>${mv.overview}</p>`;        
+        str +=       `<p><a href="#" class="btn btn-primary" role="button">Review</a> <a href="#" class="btn btn-default" role="button">Watch List</a></p>`;
+        str +=     `</div>`;
+        str +=   `</div>`;
+        str += `</div>`;
+        if (i % 3 === 2 || i === movies.length - 1) {
+            str += `</div>`;
+        }
+    });
+    printToDom(str);
+};
+
+const printToDom = (mvs) => {
+    $('#movies').append(mvs);
+};
+
+module.exports = {domString};
